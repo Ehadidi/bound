5c09ec75cec9fe0871b3211080823639efa48860
abdallah279 <abdallahzeko8@gmail.com>
2024-02-12 13:09:17 +0200

    about slider

--- pages/about.vue
+++ pages/about.vue
@@ -118,17 +118,7 @@ import { response } from "~/network/response";
 const axios = useNuxtApp().$axios;
 
 // slider
-const slider = ref([
-    {
-        image: 'https://www.thespruce.com/thmb/S1aye-s9z6VRA58-V8oRLSQwKCc=/5100x0/filters:no_upscale():max_bytes(150000):strip_icc()/SPR-luxury-kitchens-5211364-hero-688d716970544978bc12abdf17ce6f83.jpg'
-    },
-    {
-        image: 'https://planner5d.com/blog/content/images/2022/11/luxury-kitchen-with-gold-elements.jpg'
-    },
-    {
-        image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/modern-kitchen-ideas-navy-blue-1577119453.jpg'
-    },
-]);
+const slider = ref([]);
 
 const responsiveOptions = ref([
     {
